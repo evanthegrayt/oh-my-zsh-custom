ffd897cabb798c5cbc90484d71dea86ec914f00b
Evan Gray <egray1985@gmail.com>
2018-10-22 09:10:03 -0500

    Add vb function and exercism_completion

---
+++ vb.zsh
@@ -0,0 +1,45 @@
+# Function to simplify using two vagrant boxes
+# Probably only useful to me...
+
+vb() {
+    local box
+    local cmd="$1"
+    local cache="$ZSH/cache/vb.cache"
+    local wf_dir="$HOME/workflow/"
+    local boxes=(vagrant-{ique,dotcom})
+    local current_vagrant_box=$( cat $cache )
+
+    case "$cmd" in
+        up)   cd $wf_dir/$current_vagrant_box && vagrant up   ;;
+        ssh)  cd $wf_dir/$current_vagrant_box && vagrant ssh  ;;
+        halt) cd $wf_dir/$current_vagrant_box && vagrant halt ;;
+        list)
+            for box in ${boxes[@]}; do
+                if [[ $box == $current_vagrant_box ]]; then
+                    echo "* $box"
+                else
+                    echo "  $box"
+                fi
+            done
+            ;;
+        switch)
+            case $current_vagrant_box in
+                vagrant-ique)   box=vagrant-dotcom       ;;
+                vagrant-dotcom) box=vagrant-ique         ;;
+                *) echo "[CURRENT_VAGRANT_BOX] not set!" ;;
+            esac
+            echo $box > $cache
+            echo "Switching to box [$box]"
+            ;;
+        *)
+            [[ -n "$cmd" ]] && echo "Invalid command [$cmd]"
+            echo "Available commands are:"
+            echo "    up"
+            echo "    ssh"
+            echo "    halt"
+            echo "    list"
+            echo "    switch"
+            ;;
+    esac
+}
+
