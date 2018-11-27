ad33b51be0f703faaa64506a2b0363c703780fef
Evan Gray <egray1985@gmail.com>
2018-11-27 09:05:37 -0600

    Add subdirectory support

--- plugins/cdc/cdc.zsh
+++ plugins/cdc/cdc.zsh
@@ -1,11 +1,16 @@
 # `cd` to my repos in my repo directories. I decided to do this, rather than
 # adding to $CDPATH, because I don't like changing the default `cd` behavior.
+# Not only so, but a lot of the repositories have the same
 
 cdc() {
 
     local dir
-    local cd_dir="$1"
+    local cd_dir="${1%%/*}"
     local USAGE="$0: [DIRECTORY]"
+
+    if [[ "$1" == */* ]]; then
+        local subdir="${1#*/}"
+    fi
 
     if (( ${#REPO_DIRS[@]} == 0 )); then
         print "You must \`export REPO_DIRS=()\`!" >&2
@@ -18,12 +23,18 @@ cdc() {
     for dir in ${REPO_DIRS[@]}; do
         if [[ -d $dir ]]; then
             if [[ -d $dir/$cd_dir ]]; then
-                cd $dir/$cd_dir
+                cd "$dir/$cd_dir"
+                if [[ -n $subdir ]]; then
+                    if [[ -d $subdir ]]; then
+                        cd "$subdir"
+                    else
+                        print "Directory [$subdir] does not exist in [$cd_dir]."
+                    fi
+                fi
                 return 0
             fi
         else
-            print "[$dir] is exported in \$REPO_DIRS, " \
-                "but it's not a directory!" >&2
+            print "[$dir] is exported in \$REPO_DIRS but is not a directory" >&2
         fi
     done
 
